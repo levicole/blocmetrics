eb87a1d1ce7c01fa0c163d9155ff2d81d5d74504
Austin Archibald <austin.archibald@gmail.com>
2015-06-16 04:04:27 -0600

    applications model,view, and controller with CRUD

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,44 @@
+class ApplicationsController < ApplicationController
+  def show
+    @user = current_user
+    @applications = @user.applications
+    @application = Application.new
+  end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = current_user.applications.build(app_params)
+    @new_application = Application.new
+    if @application.save
+      flash.now[:notice] = "Application registered."
+    else
+      flash[:error] = "There was an error registering the application. Try again."
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @application = current_user.applications.find(params[:id])
+    if @application.destroy
+      flash.now[:notice] = "Application removed."
+    else
+      flash[:error] = "There was an error removing the application."
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def app_params
+    params.require(:application).permit(:name, :url)
+  end
+end
